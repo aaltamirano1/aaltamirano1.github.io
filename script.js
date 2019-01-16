fbbd50e8f549c50fd63e3e1cd9bd37a3e7aedcce
Angel Altamirano <angel.g.altam@gmail.com>
2019-01-15 20:09:03 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,126 +1,89 @@
+function watchContactLink(){
+	$("#contactLink").on('click', ()=>{
+		$("main, section").hide();
+		$("#booking, footer").css("display", "flex");
+	});
+}
 
-$("section").css("display", "none");
+function generateProjectHtml(projects, i){
+let demoLink = projects[i].demoUrl ? `<button><a target="_blank" href="${projects[i].demoUrl}">View Demo</a></button>` : '';
 
-$("#homeLink").click(function(){
-	$("main")[0].style.display = "initial";
-	$("footer")[0].style.display = "flex";
+let html = `
+	<div id="project-pic" style="background:url(${projects[i].imageUrl}); background-size:cover;"></div>
+  <div id="project-info">
+    <h2 id="project-name">${projects[i].name}</h2>
+    <p style="font-family: 'Open Sans', serif;margin-bottom: 9px;">${projects[i].description}</p>
+    <button><a target="_blank" href="${projects[i].codeUrl}">View Code</a></button>
+    ${demoLink}
+  </div>`;
 
-	$("section").css("display", "none");
-});
+$(html).hide().appendTo("#project").fadeIn('1000');
 
-$("#bioLink").on('click', ()=>{
-	$("main, footer, section").hide();
-	$("#bio").css("display", "flex");
-});
+}
 
-$("#portfolioLink").click(function(){
-	$("main")[0].style.display = "none";
-	$("footer")[0].style.display = "flex";
-	$("section").css("display", "none");
-
-	$("#portfolio").css("display", "flex");
-});
-
-
-$("#contactLink").click(function(){
-	$("main")[0].style.display = "none";
-	$("footer")[0].style.display = "flex";
-	$("section").css("display", "none");
-
-	$("#booking").css("display", "flex");
-});
-
-var projectIndex= -1;
-
-$("main, #portfolio").on("mouseover", function(){
-	$(".arrow").css("display", "initial");
-});
-$("main, #portfolio").on("mouseout", function(){
-	$(".arrow").css("display", "none");
-});
-
-var projects = 	[
-	{
-		name: 'Node Content Scraper', 
-		description: "Program visits the website http://shirts4mike.com and uses http://shirts4mike.com/shirts.php as single entry point to scrape information for 8 t-shirts from the site. Details for each t-shirt are gathered and saved to a CSV file include the t-shirt's Title, Price, ImageURL, URL as well as the time the information was gathered. The time is in Coordinated Universal Time. The CSV file is saved to a 'data' folder and named for the day it was gathered. If no data folder exists, one is created. The program uses the npm packages Fast-CSV to create the file and Cheerio which lets you use syntax similar to jQeury from within node to scrape the data. Built from scratch.", 
-		imageUrl: 'images/node-scraper.png',
-		demoUrl: false,
-		codeUrl: 'https://github.com/aaltamirano1/node-content-scraper'
-	},
-	{
-		name: 'Nutrient Tracker', 
-		description: 'Lets users set goals to increase or cutback on nutrients and track daily performance ratings and notes. Notes are organized in journal by date/rating so users can track how these nutrients affect them when they preform well or poorly on their goals. Built from scratch with Rails, Bootstrap, jquery, a simple-calendar gem and devise gem for user-authentication.',
-		imageUrl: 'images/nutrient-tracker.png',
-		demoUrl: 'https://nutrient-tracker.herokuapp.com/',
-		codeUrl: 'https://github.com/aaltamirano1/Nutrient-Tracker'
-	},
-	{
-		name: 'Build a Front-End Site with Gulp', 
-		description: "Gulpfile built from scratch, all other project files provided. Used Gulp to concatenate and minify the JavaScript files, compile SCSS into CSS in a concatenated and minified file, generate JavaScript and CSS source maps, and compress any JPEG or PNG files. All output for the build process saved in a dist folder for distribution or deployment.", 
-		imageUrl: 'images/gulp.png',
-		demoUrl: false,
-		codeUrl: 'https://github.com/aaltamirano1/techdegree-project-8'
-	},
-	{
-		name: 'Employee Directory', 
-		description:	'Used a public API (randomuser.me) to create an employee directory from scratch with JavaScript and jQuery. Data requested using fetch method. Directory includes an employee search function and a modal window with more detailed employee information.', 
-		imageUrl:	'images/employee-directory.png',
-		demoUrl:	'https://aaltamirano1.github.io/techdegree-project-5/',
-		codeUrl:	'https://github.com/aaltamirano1/techdegree-project-5'
-	},
-	{
-		name: 'Pagination and Content Filter', 
-		description:	'Used JavaScript and jQuery to dynamically divide the contents (a list of students) into pages and added a search function to list only the students that match that search. JavaScript file built from scratch. Search function and pagination links disabled if JavaScript is turned off; entire list of students is displayed.', 
-		imageUrl:	'images/pagination.png',
-		demoUrl:	'https://aaltamirano1.github.io/pagination-content-filter/',
-		codeUrl:	'https://github.com/aaltamirano1/pagination-content-filter'
-	},
-	{
-		name: "Conway's Game of Life", 
-		description:	'The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. Built with JavaScript and jQuery.', 
-		imageUrl:	'images/conways.png',
-		demoUrl:	'https://aaltamirano1.github.io/conways-game-of-life/',
-		codeUrl:	'https://github.com/aaltamirano1/conways-game-of-life' 
-	}
-];
-
-$(document).click("#portfolio .left-arrow", function(){
-	if(projectIndex<0){
-		projectIndex--;
-		$("#project").empty();
-
-		let demoLink = '';
-		if (projects[projectIndex].demoUrl){
-			demoLink = `<button><a target="_blank" href="${projects[projectIndex].demoUrl}">View Demo</a></button>`;
+function watchMouseover(){
+	var projectIndex =-1;
+	$("#portfolio")
+	.on("click",".left-arrow", function(){
+		if(projectIndex>0){
+			projectIndex--;
+			$("#project").empty();
+			generateProjectHtml(PROJECTS, projectIndex);
 		}
-		$("#project").html(`
-			<div id="project-pic" style="background:url(${projects[projectIndex].imageUrl}); background-size:cover;"></div>
-      <div id="project-info">
-        <h2 id="project-name">${projects[projectIndex].name}</h2>
-        <p style="font-family: 'Open Sans', serif;margin-bottom: 9px;">${projects[projectIndex].description}</p>
-        <button><a target="_blank" href="${projects[projectIndex].codeUrl}">View Code</a></button>
-        ${demoLink}
-      </div>
-		`);
-	}
-});
-$(document).on("click", "#portfolio .right-arrow", function(){
-	if (projectIndex!=projects.length-1){
-		projectIndex++;
-		$("#project").empty();
-
-		let demoLink = '';
-		if (projects[projectIndex].demoUrl){
-			demoLink = `<button><a target="_blank" href="${projects[projectIndex].demoUrl}">View Demo</a></button>`;
+		if(projectIndex==0){
+			$(this).hide();
 		}
-		$("#project").html(`
-			<div id="project-pic" style="background:url(${projects[projectIndex].imageUrl}); background-size:cover;"></div>
-      <div id="project-info">
-        <h2 id="project-name">${projects[projectIndex].name}</h2>
-        <p style="font-family: 'Open Sans', serif;margin-bottom: 9px;">${projects[projectIndex].description}</p>
-        <button><a target="_blank" href="${projects[projectIndex].codeUrl}">View Code</a></button>
-        ${demoLink}
-      </div>
-    `);
-	}
-});
+		if(projectIndex==4){
+			$(".right-arrow").show();
+		}
+	})
+	.on("click",".right-arrow", function(){
+		let lastProject = PROJECTS.length-1;
+		if (projectIndex!= lastProject){
+			projectIndex++;
+			$("#project").empty();
+			generateProjectHtml(PROJECTS, projectIndex);
+		}
+		if (projectIndex==lastProject){
+			$(this).hide();
+		}
+		if(projectIndex==1){
+			$(".left-arrow").show();
+		}
+	});
+}
+
+function watchPortfolioLink(){
+	$("#portfolioLink").on('click', ()=>{
+		$("main, section").hide();
+		$("#portfolio, footer").css("display", "flex");
+
+		$(".right-arrow").show();
+	});
+	watchMouseover();
+
+}
+
+function watchBioLink(){
+	$("#bioLink").on('click', ()=>{
+		$("main, footer, section").hide();
+		$("#bio").css("display", "flex");
+	});
+}
+
+function watchHomeLink(){
+	$("#homeLink").on('click', ()=>{
+		$("main").show()
+		$("footer").css("display","flex");
+		$("section").hide();
+	});
+}
+
+function initialDisplay(){
+	watchHomeLink();
+	watchBioLink();
+	watchPortfolioLink();
+	watchContactLink();
+}
+
+$(initialDisplay);
